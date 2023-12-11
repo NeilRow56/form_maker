e5031648594e4205ebbfda4c930d8e1c763fb0b0
NeilRow56 <neilrowland56@gmail.com>
2023-12-11 07:57:42 +0000

    Initial layout of Desiner and designer sidebar

--- src/components/shared/Designer.tsx
+++ src/components/shared/Designer.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import DesignerSidebar from './DesignerSidebar'
 
 function Designer() {
-  return <div>Designer component</div>
+  return (
+    <div className="flex h-full w-full">
+      <div className="w-full p-4">
+        <div className="m-auto flex h-full max-w-[920px] flex-1 flex-grow flex-col items-center justify-start overflow-y-auto rounded-xl bg-background">
+          <p className="flex flex-grow items-center text-3xl font-bold text-muted-foreground">
+            Drop here
+          </p>
+        </div>
+      </div>
+      <DesignerSidebar />
+    </div>
+  )
 }
 
 export default Designer
---
+++ src/components/shared/DesignerSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DesignerSidebar() {
+  return (
+    <aside className="flex w-[400px] max-w-[400px] flex-grow flex-col gap-2 overflow-y-auto border-l-2 border-muted bg-background p-4">
+      Sidebar
+    </aside>
+  )
+}
